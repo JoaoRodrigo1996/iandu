35285283535594f9ed1bacb7905eb2a68fcab4ae
Rodrigo Mesquita <dev.rodrigomesquita@gmail.com>
2025-02-27 11:40:07 -0300

    create company entity

---
+++ src/domain/scheduling/enterprise/entities/company.ts
@@ -0,0 +1,120 @@
+import type { Optional } from '@/core/types/optional'
+import { Entity } from '../../../../core/entities/entity'
+import type { UniqueEntityID } from '../../../../core/entities/unique-entity-id'
+
+interface CompanyProps {
+  ownerId: string
+  name: string
+  cnpj: string
+  address: {
+    street: string
+    number: number
+    complement: string
+    neighborhood: string
+    city: string
+    state: string
+    zip: string
+  }
+  email: string
+  phone: string
+  description: string
+  sector: string
+  createdAt: Date
+  updatedAt?: Date
+}
+
+export class Company extends Entity<CompanyProps> {
+  get ownerId() {
+    return this.props.ownerId
+  }
+
+  get name() {
+    return this.props.name
+  }
+
+  set name(name: string) {
+    this.props.name = name
+    this.touch()
+  }
+
+  get cnpj() {
+    return this.props.cnpj
+  }
+
+  set cnpj(cnpj: string) {
+    this.props.cnpj = cnpj
+    this.touch()
+  }
+
+  get address() {
+    return this.props.address
+  }
+
+  set address(address: CompanyProps['address']) {
+    this.props.address = address
+    this.touch()
+  }
+
+  get email() {
+    return this.props.email
+  }
+
+  set email(email: string) {
+    this.props.email = email
+    this.touch()
+  }
+
+  get phone() {
+    return this.props.phone
+  }
+
+  set phone(phone: string) {
+    this.props.phone = phone
+    this.touch()
+  }
+
+  get description() {
+    return this.props.description
+  }
+
+  set description(description: string) {
+    this.props.description = description
+    this.touch()
+  }
+
+  get sector() {
+    return this.props.sector
+  }
+
+  set sector(sector: string) {
+    this.props.sector = sector
+    this.touch()
+  }
+
+  get createdAt() {
+    return this.props.createdAt
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt
+  }
+
+  private touch() {
+    this.props.updatedAt = new Date()
+  }
+
+  static create(
+    props: Optional<CompanyProps, 'createdAt'>,
+    id?: UniqueEntityID
+  ) {
+    const company = new Company(
+      {
+        ...props,
+        createdAt: props.createdAt ?? new Date(),
+      },
+      id
+    )
+
+    return company
+  }
+}
