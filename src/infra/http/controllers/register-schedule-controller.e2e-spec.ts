7e880b465317e55955971dff2a7f1e4db00857e1
Rodrigo Mesquita <dev.rodrigomesquita@gmail.com>
2025-08-26 11:05:10 -0300

    refactor register schedule controller e2e test.

--- src/infra/http/controllers/register-schedule-controller.e2e-spec.ts
+++ src/infra/http/controllers/register-schedule-controller.e2e-spec.ts
@@ -1,15 +1,24 @@
 import { app } from '@/infra/app'
+import { JwtEncrypter } from '@/infra/cryptography/jwt-encrypter'
 import { PrismaService } from '@/infra/database/prisma'
 import { faker } from '@faker-js/faker'
 import { hash } from 'bcrypt'
 import request from 'supertest'
+import { ClientFactory } from 'test/factories/make-client'
+import { OrganizationFactory } from 'test/factories/make-organization'
 
-let prismaService: PrismaService
+let prisma: PrismaService
+let jwtService: JwtEncrypter
+let clientFactory: ClientFactory
+let organizationFactory: OrganizationFactory
 
 describe('Create schedule', () => {
   beforeAll(async () => {
     app.ready()
-    prismaService = new PrismaService()
+    prisma = new PrismaService()
+    jwtService = new JwtEncrypter(app)
+    clientFactory = new ClientFactory(prisma)
+    organizationFactory = new OrganizationFactory(prisma)
   })
 
   afterAll(async () => {
@@ -17,54 +26,19 @@ describe('Create schedule', () => {
   })
 
   it('[POST] /schedule/:organizationId - should be able to create new schedule', async () => {
-    const client = {
-      name: faker.person.fullName(),
-      email: faker.internet.email(),
-      userName: faker.internet.username(),
-    }
-
-    await request(app.server).post('/users').send({
-      name: client.name,
-      email: client.email,
-      userName: client.userName,
-      password: '12345678',
+    const client = await clientFactory.makePrismaClient()
+    const access_token = await jwtService.encrypt({ sub: client.id.toString() })
+    const organization = await organizationFactory.makePrismaOrganization({
+      clientId: client.id,
     })
 
-    const authResponse = await request(app.server).post('/sessions').send({
-      email: client.email,
-      password: '12345678',
-    })
-
-    const { access_token } = await authResponse.body
-
-    const organization = await request(app.server)
-      .post('/organization')
-      .set('Authorization', `Bearer ${access_token}`)
-      .send({
-        name: 'Amazon',
-        address: {
-          city: 'Miami',
-          complement: 'Apt 123',
-          neighborhood: 'Downtown',
-          number: 123,
-          state: 'FL',
-          street: 'Main St',
-          zip: '12345',
-        },
-        email: faker.internet.email(),
-        cnpj: `${faker.string.numeric(14)}`,
-        description: 'A organization description',
-        sector: 'Services',
-        phone: '(99) 99999-9999',
-      })
-
-    const organizationId = organization.body.organization.id.value
+    const organizationId = organization.id.toString()
 
     const response = await request(app.server)
       .post(`/schedule/${organizationId}`)
       .set('Authorization', `Bearer ${access_token}`)
       .send({
-        date: new Date('2022-01-01T13:00:00.000Z'),
+        date: new Date('2025-09-09T13:00:00.000Z'),
       })
 
     expect(response.statusCode).toBe(201)
