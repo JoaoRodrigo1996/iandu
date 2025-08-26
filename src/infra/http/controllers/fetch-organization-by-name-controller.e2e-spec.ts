7ba0716a6907917f5ab8d06881c818796ecf06e7
Rodrigo Mesquita <dev.rodrigomesquita@gmail.com>
2025-08-26 10:46:52 -0300

    refactor fetch organization by name controller e2e test.

--- src/infra/http/controllers/fetch-organization-by-name-controller.e2e-spec.ts
+++ src/infra/http/controllers/fetch-organization-by-name-controller.e2e-spec.ts
@@ -1,14 +1,22 @@
 import { app } from '@/infra/app'
+import { JwtEncrypter } from '@/infra/cryptography/jwt-encrypter'
 import { PrismaService } from '@/infra/database/prisma'
-import { faker } from '@faker-js/faker'
 import request from 'supertest'
+import { ClientFactory } from 'test/factories/make-client'
+import { OrganizationFactory } from 'test/factories/make-organization'
 
 let prisma: PrismaService
+let clientFactory: ClientFactory
+let jwtService: JwtEncrypter
+let organizationFactory: OrganizationFactory
 
 describe('Fetch organization by name', () => {
   beforeAll(async () => {
     app.ready()
     prisma = new PrismaService()
+    jwtService = new JwtEncrypter(app)
+    clientFactory = new ClientFactory(prisma)
+    organizationFactory = new OrganizationFactory(prisma)
   })
 
   afterAll(async () => {
@@ -16,50 +24,15 @@ describe('Fetch organization by name', () => {
   })
 
   it('[GET] /organization - should be able to fetch organization by name', async () => {
-    const client = {
-      name: faker.person.fullName(),
-      email: faker.internet.email(),
-      userName: faker.internet.username(),
-    }
-
-    await request(app.server).post('/users').send({
-      name: client.name,
-      email: client.email,
-      userName: client.userName,
-      password: '12345678',
+    const client = await clientFactory.makePrismaClient()
+    const access_token = await jwtService.encrypt({ sub: client.id.toString() })
+    const organization = await organizationFactory.makePrismaOrganization({
+      clientId: client.id,
     })
-
-    const authResponse = await request(app.server).post('/sessions').send({
-      email: client.email,
-      password: '12345678',
-    })
-
-    const { access_token } = await authResponse.body
-
-    await request(app.server)
-      .post('/organization')
-      .set('Authorization', `Bearer ${access_token}`)
-      .send({
-        name: 'Amazon',
-        address: {
-          city: 'Miami',
-          complement: 'Apt 123',
-          neighborhood: 'Downtown',
-          number: 123,
-          state: 'FL',
-          street: 'Main St',
-          zip: '12345',
-        },
-        email: faker.internet.email(),
-        cnpj: `${faker.string.numeric(14)}`,
-        description: 'A organization description',
-        sector: 'Services',
-        phone: '(99) 99999-9999',
-      })
 
     const response = await request(app.server)
       .get('/organization')
-      .query({ name: 'Amazon' })
+      .query({ name: organization.name })
       .set('Authorization', `Bearer ${access_token}`)
       .send()
 
@@ -67,7 +40,7 @@ describe('Fetch organization by name', () => {
     expect(response.body).toEqual(
       expect.objectContaining({
         organization: expect.objectContaining({
-          name: 'Amazon',
+          name: organization.name,
         }),
       })
     )
