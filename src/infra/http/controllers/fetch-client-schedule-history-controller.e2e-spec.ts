48a0da33f9620f83c71f57898f9624dcf4a0708c
Rodrigo Mesquita <dev.rodrigomesquita@gmail.com>
2025-08-26 10:42:42 -0300

    refactor fetch client schedule history controller e2e test.

--- src/infra/http/controllers/fetch-client-schedule-history-controller.e2e-spec.ts
+++ src/infra/http/controllers/fetch-client-schedule-history-controller.e2e-spec.ts
@@ -1,14 +1,25 @@
 import { app } from '@/infra/app'
+import { JwtEncrypter } from '@/infra/cryptography/jwt-encrypter'
 import { PrismaService } from '@/infra/database/prisma'
-import { faker } from '@faker-js/faker'
 import request from 'supertest'
+import { ClientFactory } from 'test/factories/make-client'
+import { OrganizationFactory } from 'test/factories/make-organization'
+import { ScheduleFactory } from 'test/factories/make-scheduling'
 
 let prisma: PrismaService
+let clientFactory: ClientFactory
+let jwtService: JwtEncrypter
+let organizationFactory: OrganizationFactory
+let scheduleFactory: ScheduleFactory
 
 describe('Fetch client schedule history', () => {
   beforeAll(async () => {
     app.ready()
     prisma = new PrismaService()
+    jwtService = new JwtEncrypter(app)
+    clientFactory = new ClientFactory(prisma)
+    organizationFactory = new OrganizationFactory(prisma)
+    scheduleFactory = new ScheduleFactory(prisma)
   })
 
   afterAll(async () => {
@@ -16,55 +27,15 @@ describe('Fetch client schedule history', () => {
   })
 
   it('[GET] /schedule/history - should be able to fetch client schedule history', async () => {
-    const client = {
-      name: faker.person.fullName(),
-      email: faker.internet.email(),
-      userName: faker.internet.username(),
-    }
-
-    await request(app.server).post('/users').send({
-      name: client.name,
-      email: client.email,
-      userName: client.userName,
-      password: '12345678',
+    const client = await clientFactory.makePrismaClient()
+    const access_token = await jwtService.encrypt({ sub: client.id.toString() })
+    const organization = await organizationFactory.makePrismaOrganization({
+      clientId: client.id,
     })
-
-    const authResponse = await request(app.server).post('/sessions').send({
-      email: client.email,
-      password: '12345678',
+    await scheduleFactory.makePrismaSchedule({
+      clientId: client.id,
+      organizationId: organization.id,
     })
-
-    const { access_token } = await authResponse.body
-
-    const organization = await request(app.server)
-      .post('/organization')
-      .set('Authorization', `Bearer ${access_token}`)
-      .send({
-        name: 'Amazon',
-        address: {
-          city: 'Miami',
-          complement: 'Apt 123',
-          neighborhood: 'Downtown',
-          number: 123,
-          state: 'FL',
-          street: 'Main St',
-          zip: '12345',
-        },
-        email: faker.internet.email(),
-        cnpj: `${faker.string.numeric(14)}`,
-        description: 'A organization description',
-        sector: 'Services',
-        phone: '(99) 99999-9999',
-      })
-
-    const organizationId = organization.body.organization.id.value
-
-    await request(app.server)
-      .post(`/schedule/${organizationId}`)
-      .set('Authorization', `Bearer ${access_token}`)
-      .send({
-        date: new Date('2022-01-01T13:00:00.000Z'),
-      })
 
     const response = await request(app.server)
       .get('/schedule/history')
