6ff81734d9ad6f54725fe35c07b1069d28b63b52
Rodrigo Mesquita <dev.rodrigomesquita@gmail.com>
2025-08-26 10:32:33 -0300

    refactor create organization controller e2e test.

--- src/infra/http/controllers/create-organization-controller.e2e-spec.ts
+++ src/infra/http/controllers/create-organization-controller.e2e-spec.ts
@@ -1,15 +1,20 @@
 import { app } from '@/infra/app'
+import { JwtEncrypter } from '@/infra/cryptography/jwt-encrypter'
 import { PrismaService } from '@/infra/database/prisma'
 import { faker } from '@faker-js/faker'
-import { hash, hashSync } from 'bcrypt'
 import request from 'supertest'
+import { ClientFactory } from 'test/factories/make-client'
 
-let prismaService: PrismaService
+let prisma: PrismaService
+let clientFactory: ClientFactory
+let jwtService: JwtEncrypter
 
 describe('Create organization', () => {
   beforeAll(async () => {
     await app.ready()
-    prismaService = new PrismaService()
+    prisma = new PrismaService()
+    jwtService = new JwtEncrypter(app)
+    clientFactory = new ClientFactory(prisma)
   })
 
   afterAll(async () => {
@@ -17,25 +22,8 @@ describe('Create organization', () => {
   })
 
   it('[POST] /organization - should be able to create new organization', async () => {
-    const client = {
-      name: faker.person.fullName(),
-      email: faker.internet.email(),
-      userName: faker.internet.username(),
-    }
-
-    await request(app.server).post('/users').send({
-      name: client.name,
-      email: client.email,
-      userName: client.userName,
-      password: '12345678',
-    })
-
-    const authResponse = await request(app.server).post('/sessions').send({
-      email: client.email,
-      password: '12345678',
-    })
-
-    const { access_token } = await authResponse.body
+    const client = await clientFactory.makePrismaClient()
+    const access_token = await jwtService.encrypt({ sub: client.id.toString() })
 
     const response = await request(app.server)
       .post('/organization')
